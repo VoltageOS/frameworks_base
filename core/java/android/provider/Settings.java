509f20a9079401c654cd67f03dca70fbdca33b1a
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    SystemUI: Allow to enable Lockscreen Media Art
    This is a squash of the following commits:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Fri Oct 23 21:07:35 2015 -0400
    
        SystemUI: Allow to enable Lockscreen Media Art
    
        Change-Id: Id93e115649df5d6dc680101a8c852644b217c28a
    
    Author: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Date:   Fri Nov 20 00:41:04 2020 +0200
    
        SystemUI: Hide lockscreen media art if media is not playing
    
        it's annoying to get the artwork even if the notification gets
        swiped out and media is paused, because the media session is still alive
    
        Change-Id: Ie65932127e47c463ef9a8b763298fc4dae20f8ae
    
    Change-Id: Idd3033b52b4aa1cf07eb694ea0c9c1c934248f7c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    fixup! SystemUI: Allow to enable Lockscreen Media Art
    
    Change-Id: I5d670128ba7d66a575f5ed751ba8c9ce2a2dcbb7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5812,6 +5812,12 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Switch for lockscreen media metadata
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

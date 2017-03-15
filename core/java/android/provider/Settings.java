c8e5042997d7765511513425b95281a5c4128549
Pranav Vashi <neobuddy89@gmail.com>
2017-03-15 07:36:19 +0530

    base: Make roaming indicator optional [1/2]
    * Roaming indicator on signal icon is useless most of the times
      since a carrier might not charge differently to user if in same
      country.
    * So, allow user to hide roaming indicator and see what kind of signal
      strength is available to him.
    * This commit will only allow to hide indicator not roaming warnings.
    
    Change-Id: Ia048b08f20f1e2fe276ee0da112f9e2a892fee07
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5853,6 +5853,11 @@ public final class Settings {
         public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
 
         /**
+         * @hide
+         */
+        public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

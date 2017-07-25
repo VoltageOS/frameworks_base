a45db3d06dd05c806c00d043f74b83820b9f3c97
Daniel Micay <danielmicay@gmail.com>
2017-07-25 11:22:33 -0400

    add system property for disabling keyguard camera
    Change-Id: I0b65cac3c3d2fc495b339c34add742bd698b107c
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -6,6 +6,9 @@ import java.util.Set;
 /** @hide */
 public class ExtSettings {
 
+    public static final BoolSetting ALLOW_KEYGUARD_CAMERA = new BoolSetting(
+            Setting.Scope.SYSTEM_PROPERTY, "persist.keyguard.camera", true);
+
     public static final BoolSetting AUTO_GRANT_OTHER_SENSORS_PERMISSION = new BoolSetting(
             Setting.Scope.PER_USER, "auto_grant_OTHER_SENSORS_perm", true);
 

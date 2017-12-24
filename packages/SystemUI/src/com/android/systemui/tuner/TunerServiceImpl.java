1c95f5699a0ede340fe02d3355f208c86e2f8591
Pranav Vashi <neobuddy89@gmail.com>
2017-12-24 11:29:10 +0530

    SystemUI: Allow using tuner API for Global settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -180,8 +180,12 @@ public class TunerServiceImpl extends TunerService {
         return key.startsWith("system:");
     }
 
+    private boolean isGlobal(String key) {
+        return key.startsWith("global:");
+    }
+
     private String chomp(String key) {
-        return key.replaceFirst("^(customglobal|customsecure|customsystem|system):", "");
+        return key.replaceFirst("^(customglobal|customsecure|customsystem|system|global):", "");
     }
 
     @Override
@@ -196,6 +200,9 @@ public class TunerServiceImpl extends TunerService {
                     mContentResolver, chomp(setting), mCurrentUser);
         } else if (isSystem(setting)) {
             return Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isGlobal(setting)) {
+            return Settings.Global.getStringForUser(
                     mContentResolver, chomp(setting), mCurrentUser);
         } else {
             return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
@@ -215,6 +222,9 @@ public class TunerServiceImpl extends TunerService {
         } else if (isSystem(setting)) {
             Settings.System.putStringForUser(
                     mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isGlobal(setting)) {
+            Settings.Global.putStringForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
         } else {
             Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
         }
@@ -233,6 +243,9 @@ public class TunerServiceImpl extends TunerService {
         } else if (isSystem(setting)) {
             return Settings.System.getIntForUser(
                     mContentResolver, chomp(setting), def, mCurrentUser);
+        } else if (isGlobal(setting)) {
+            return Settings.Global.getInt(
+                    mContentResolver, chomp(setting), def);
         } else {
             return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
         }
@@ -251,6 +264,9 @@ public class TunerServiceImpl extends TunerService {
                     mContentResolver, chomp(setting), mCurrentUser);
         } else if (isSystem(setting)) {
             ret = Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isGlobal(setting)) {
+            ret = Settings.Global.getStringForUser(
                     mContentResolver, chomp(setting), mCurrentUser);
         } else {
             ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
@@ -271,6 +287,8 @@ public class TunerServiceImpl extends TunerService {
                     mContentResolver, chomp(setting), value, mCurrentUser);
         } else if (isSystem(setting)) {
             Settings.System.putIntForUser(mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isGlobal(setting)) {
+            Settings.Global.putInt(mContentResolver, chomp(setting), value);
         } else {
             Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
         }
@@ -301,6 +319,8 @@ public class TunerServiceImpl extends TunerService {
             uri = Settings.System.getUriFor(chomp(key));
         } else if (isSystem(key)) {
             uri = Settings.System.getUriFor(chomp(key));
+        } else if (isGlobal(key)) {
+            uri = Settings.Global.getUriFor(chomp(key));
         } else {
             uri = Settings.Secure.getUriFor(key);
         }

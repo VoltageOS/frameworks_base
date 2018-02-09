80843be3f3712630346ac9dc8457fa3ce88db036
ezio84 <brabus84@gmail.com>
2018-02-09 18:20:23 +0100

    Add Google Sound Search tile
     * SoundSearchTile: Make default state inactive
     * Disable long click for SoundSearch tile
    
    Change-Id: Id893722185fa4f111f2730849d1c8abfffa121dc
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -59,6 +59,7 @@ import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SleepModeTile;
+import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
@@ -118,6 +119,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<RebootTile> mRebootTileProvider;
     private final Provider<VolumeTile> mVolumeTileProvider;
 	private final Provider<DataSwitchTile> mDataSwitchTileProvider;
+    private final Provider<SoundSearchTile> mSoundSearchTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -165,7 +167,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<HeadsUpTile> headsUpTileProvider,
             Provider<RebootTile> rebootTileProvider,
             Provider<VolumeTile> volumeTileProvider,
-			Provider<DataSwitchTile> dataSwitchTileProvider) {
+			Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<SoundSearchTile> soundSearchTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -209,6 +212,7 @@ public class QSFactoryImpl implements QSFactory {
         mRebootTileProvider = rebootTileProvider;
         mVolumeTileProvider = volumeTileProvider;
 		mDataSwitchTileProvider = dataSwitchTileProvider;
+        mSoundSearchTileProvider = soundSearchTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -304,6 +308,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mVolumeTileProvider.get();
 			case "dataswitch":
                 return mDataSwitchTileProvider.get();
+            case "soundsearch":
+                return mSoundSearchTileProvider.get();
         }
 
         // Custom tiles

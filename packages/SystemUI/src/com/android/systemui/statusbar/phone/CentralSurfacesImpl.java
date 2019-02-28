96d8ba6d68fa37385c3eafb6ae8062d99b4545b4
ezio84 <brabus84@gmail.com>
2019-02-27 21:13:01 -0600

    ActionUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    [Stallix]
    * Adapt to A13 QPR2 (after 4c9824b2fd79a5e7c5c3accbccea4b01017c7ddb).
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2045,6 +2045,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mMessageRouter.sendMessage(MSG_OPEN_SETTINGS_PANEL);
     }
 
+    public void toggleSettingsPanel() {
+        if (mPanelExpanded) {
+            mShadeController.animateCollapseShade();
+        } else {
+            animateExpandSettingsPanel(null);
+        }
+    }
+
     @Override
     public boolean isPanelExpanded() {
         return mPanelExpanded;

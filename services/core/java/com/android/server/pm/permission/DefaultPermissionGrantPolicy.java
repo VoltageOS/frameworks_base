62a5d6b116f13c3021a2498453b0fa159bfbd40c
Ali B <abittin@gmail.com>
2019-06-13 16:39:15 +0300

    core: move OmniJaws client to its new home
    Also includes
    	Grant default location permissions to OmniJaws
    	Author: Ali B.
    	Change-Id: I2988eea61414dafc94e52729b997d07f4673a9ad
    
    Change-Id: I50349a910aa604959def70377da135c6d94c7774
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -33,6 +33,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManagerInternal;
+import android.content.pm.PackageParser;
 import android.content.pm.PermissionInfo;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ResolveInfo;
@@ -69,6 +70,7 @@ import com.android.internal.R;
 import com.android.internal.gmscompat.GmsCompatApp;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.XmlUtils;
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.server.LocalServices;
 import com.android.server.ServiceThread;
 import com.android.server.pm.permission.LegacyPermissionManagerInternal.PackagesProvider;
@@ -889,6 +891,10 @@ final class DefaultPermissionGrantPolicy {
         grantPermissionsToSystemPackage(pm, "com.android.vending", userId, CONTACTS_PERMISSIONS,
                 ALWAYS_LOCATION_PERMISSIONS, NEARBY_DEVICES_PERMISSIONS, PHONE_PERMISSIONS, SMS_PERMISSIONS,
                 STORAGE_PERMISSIONS);
+
+        // OmniJaws
+        String omnijawsServicePackageName = "org.omnirom.omnijaws";
+        grantSystemFixedPermissionsToSystemPackage(pm, omnijawsServicePackageName, userId, ALWAYS_LOCATION_PERMISSIONS);
 
         // ThemePicker
         grantSystemFixedPermissionsToSystemPackage(pm, "com.android.wallpaper", userId, STORAGE_PERMISSIONS);

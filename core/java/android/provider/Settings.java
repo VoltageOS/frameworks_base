31437b1f3c6e7a60515a54ca608ee05091cb8517
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: navigation mode settings [1/2]
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5544,6 +5544,9 @@ public final class Settings {
          */
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.

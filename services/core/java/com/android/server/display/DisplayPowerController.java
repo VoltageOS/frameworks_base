084c0bef394d61e7233d893b004d84efa0a7780e
sooti <sooti85@gmail.com>
2020-09-21 18:53:23 +0530

    DisplayPowerController: Fix incall proximity not turning off display
    Change-Id: I2f5c8d22f1c1d19cea8d9f7bbc316a23932400c6
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -1711,6 +1711,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         if (mProximitySensor != null) {
             mProximityThreshold = Math.min(mProximitySensor.getMaximumRange(),
                     TYPICAL_PROXIMITY_THRESHOLD);
+            if (Float.isNaN(mProximityThreshold)) {
+                mProximityThreshold = 5.0f;
+            }
         }
     }
 

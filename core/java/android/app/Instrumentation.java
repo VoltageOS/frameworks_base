c403d23c9e670103b8b66b4fd16c081aa7ee1d96
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    [SQUASH] Introduce PixelPropsUtils
    That will spoof build fingerprints on some g00gle apps
    
    * Also will enable some cool stuffs like:
      - Dynamic navbar on GBoard
      - SafetyHub
    
    and others.
    
    Thanks to @kdrag0n for the original idea at https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a
    
    Change-Id: I1078e7402833fec77edb751070c5144d08c85b6c
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    keystore: Block key attestation for SafetyNet
    
    SafetyNet (part of Google Play Services) opportunistically uses
    hardware-backed key attestation via KeyStore as a strong integrity
    check. This causes SafetyNet to fail on custom ROMs because the verified
    boot key and bootloader unlock state can be detected from attestation
    certificates.
    
    As a workaround, we can take advantage of the fact that SafetyNet's
    usage of key attestation is opportunistic (i.e. falls back to basic
    integrity checks if it fails) and prevent it from getting the
    attestation certificate chain from KeyStore. This is done by checking
    the stack for DroidGuard, which is the codename for SafetyNet, and
    pretending that the device doesn't support key attestation.
    
    Key attestation has only been blocked for SafetyNet specifically, as
    Google Play Services and other apps have many valid reasons to use it.
    For example, it appears to be involved in Google's mobile security key
    ferature.
    
    Change-Id: I5146439d47f42dc6231cb45c4dab9f61540056f6
    
    * Stallix: Adapted for A13 and squash changes with their respected authors
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -67,6 +67,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.voltage.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1246,6 +1248,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        PixelPropsUtils.setProps(context);
         return app;
     }
     
@@ -1264,6 +1267,7 @@ public class Instrumentation {
         GmsCompat.maybeEnable(context);
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        PixelPropsUtils.setProps(context);
         return app;
     }
 

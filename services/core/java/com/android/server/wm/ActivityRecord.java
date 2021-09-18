f6cfff7db9ff4a73ddf63d7a2fb16ad79a42e260
Mu Xiaodong <mu.xiaodong@zte.com.cn>
2021-09-18 15:31:15 +0800

    ActivityRecord: Fix invisible unknown app blocking keyguard transition
    If that activity starts another activity and finish itself immediately,
    the app transition controller will stuck in the status caused by
    unknown app unresolved.
    
    So the activity should be removed from unknown apps once it failed to
    pause in status of invisible, finish or stopping.
    
    Test: N/A
    Change-Id: I8f6e530a79bb168f7b86f708fb2eb8b5e3e2b7c7
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -6195,6 +6195,9 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                         completeFinishing("activityPausedLocked");
                     }
                 }
+                if (isState(STOPPING) && finishing && !isVisible() && !mVisibleRequested) {
+                    mDisplayContent.mUnknownAppVisibilityController.appRemovedOrHidden(this);
+                }
             }
         }
 

13fdf075154dad3650fa3e2eca8c3f55c56f87f3
xyyx <xyyx@mail.ru>
2021-11-07 14:35:08 +0700

    LockscreenCharging: squashed (1/3)
    needed system/core patch
    based on:
    [2/2] frameworks: show charging current & voltage on lockscreen by yank555-lu
    [2/2]Base: show more battery info on lockscreen when charging by yank555-lu
    LockscreenCharging: Use double for mChargingVoltage and show as4.XV by xyyx
    Lockscreen charging: Formatting improvements by nathanchance
    
    DennySPb: adapt to A11
    
    Change-Id: I42f90b413fd1445ded00cb5769af0ce5faf15c49
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5905,6 +5905,12 @@ public final class Settings {
         public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
                 "notification_guts_kill_app_button";
 
+        /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
 	/**
          * Whether to blink flashlight for incoming calls
          * 0 = Disabled (Default)

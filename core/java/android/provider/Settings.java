cd096f9ab269de86b545e4b1678e6c7ce0c8e996
jhonboy121 <alfredmathew05@gmail.com>
2021-11-08 13:37:25 +0530

    base: check whether device is charging when alwaysOnChargingEnabled is called
    * Using settings for this is quite redundant. And if you toggle aod on charge after plugging in then aod won't turn on unless you unplug and plug again (since DOZE_ON_CHARGE_NOW is set only if aod on charge is enabled and device is plugged in"
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5622,12 +5622,6 @@ public final class Settings {
         public static final String DOZE_ON_CHARGE = "doze_on_charge";
 
         /**
-         * Hidden setting of the current atate of DOZE only when charging
-         * @hide
-         */
-        public static final String DOZE_ON_CHARGE_NOW = "doze_on_charge_now";
-
-        /**
          * Disable power menu on secure lock screens
          *
          * @hide

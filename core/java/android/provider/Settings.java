bc3564b8a3008937c2cf4683b8abba25e5eee57e
jhonboy121 <alfredmathew05@gmail.com>
2021-11-15 01:15:37 +0530

    base: allow toggling location privacy indicator[1/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10889,6 +10889,13 @@ public final class Settings {
         public static final String DISABLE_SECURE_TILES_ON_LOCKSCREEN = "disable_secure_tiles_on_lockscreen";
 
         /**
+         * Whether to show privacy indicator for location
+         * Default value is based on it's device config flag
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */

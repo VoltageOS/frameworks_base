5a50d4c51254df36f814583808532f66d47061c8
alibei <sebastiankotzias83@gmail.com>
2021-12-03 14:35:43 +0100

    timedetector: Disable lower bound check
    This does fix an issue with the automatic time setting when the time is higher than the NTP time
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/timedetector/TimeDetectorStrategyImpl.java
+++ services/core/java/com/android/server/timedetector/TimeDetectorStrategyImpl.java
@@ -437,9 +437,8 @@ public final class TimeDetectorStrategyImpl implements TimeDetectorStrategy {
 
         // Suggestion is definitely wrong if it comes before lower time bound.
         if (lowerBound.isAfter(Instant.ofEpochMilli(newUtcTime.getValue()))) {
-            Slog.w(LOG_TAG, "Suggestion points to time before lower bound, skipping it. "
+            Slog.w(LOG_TAG, "Suggestion points to time before lower bound. "
                     + "suggestion=" + suggestion + ", lower bound=" + lowerBound);
-            return false;
         }
 
         return true;

2b9c2060b6e71b42f71734a9b6ab4dfbd09c81d6
Pranav Vashi <neobuddy89@gmail.com>
2021-12-18 11:56:17 +0530

    QuickStatusBarHeader: Link date in QS statusbar with clock
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -137,6 +137,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private boolean mIsSingleCarrier;
 
     private boolean mUseCombinedQSHeader;
+    private boolean mShowClock;
     private boolean mShowDate;
 
     private final ActivityStarter mActivityStarter;
@@ -188,6 +189,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mClockView = findViewById(R.id.clock);
         mClockView.setQsHeader();
         mClockView.setOnClickListener(this);
+        mClockView.setOnLongClickListener(this);
         mDatePrivacySeparator = findViewById(R.id.space);
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
@@ -210,7 +212,6 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         updateResources();
 
         Dependency.get(TunerService.class).addTunable(this,
-                StatusBarIconController.ICON_HIDE_LIST,
                 SHOW_QS_CLOCK,
                 SHOW_QS_DATE,
                 STATUS_BAR_BATTERY_STYLE,
@@ -438,7 +439,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
 
                     @Override
                     public void onAnimationStarted() {
-                        if (mShowDate) {
+                        if (mShowClock && mShowDate) {
                             mClockDateView.setVisibility(View.VISIBLE);
                             mClockDateView.setFreezeSwitching(true);
                         }
@@ -451,7 +452,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                     @Override
                     public void onAnimationAtStart() {
                         super.onAnimationAtStart();
-                        if (mShowDate) {
+                        if (mShowClock && mShowDate) {
                             mClockDateView.setFreezeSwitching(false);
                             mClockDateView.setVisibility(View.VISIBLE);
                         }
@@ -677,15 +678,16 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     public void onTuningChanged(String key, String newValue) {
         switch (key) {
             case SHOW_QS_CLOCK:
-                boolean showClock =
+                mShowClock =
                         TunerService.parseIntegerSwitch(newValue, true);
-                mClockView.setClockVisibleByUser(showClock);
+                mClockView.setClockVisibleByUser(mShowClock);
+                mClockDateView.setVisibility(mShowClock && mShowDate ? View.VISIBLE : View.GONE);
                 break;
             case SHOW_QS_DATE:
                 mShowDate =
                         TunerService.parseIntegerSwitch(newValue, true);
                 mDateContainer.setVisibility(mShowDate ? View.VISIBLE : View.GONE);
-                mClockDateView.setVisibility(mShowDate ? View.VISIBLE : View.GONE);
+                mClockDateView.setVisibility(mShowClock && mShowDate ? View.VISIBLE : View.GONE);
                 break;
             case QS_BATTERY_STYLE:
                 mQSBatteryStyle =
@@ -730,7 +732,5 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             default:
                 break;
         }
-        mClockView.setClockVisibleByUser(!StatusBarIconController.getIconHideList(
-                mContext, newValue).contains("clock"));
     }
 }

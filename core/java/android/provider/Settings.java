527709b52c054d382f4224241bfbf4b144c6abfc
spkal01 <kalligeross@gmail.com>
2022-02-01 21:52:59 +0800

    SystemUI: Allow Configuring Navbar Radius [1/2]
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6337,6 +6337,12 @@ public final class Settings {
          */
         public static final String UDFPS_ICON = "udfps_icon";
 
+	/**
+         * Size of gesture bar radius.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.

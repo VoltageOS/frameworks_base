1677e45e1efff4e5d46f135fc10b9330c3de48b8
Alfred Mathew <alfredmathew05@gmail.com>
2022-02-22 14:19:30 +0530

    services: applock: disable for work profile apps
    * There is no need to have a lock there
    
    Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -1222,12 +1222,12 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
         final ActivityInfo aInfo = resolveActivityInfoForIntent(intent, resolvedType, userId, callingUid);
         if (aInfo != null) {
-            if (getAppLockManagerService().requireUnlock(aInfo.packageName, mContext.getUserId())) {
+            if (getAppLockManagerService().requireUnlock(aInfo.packageName, userId)) {
                 getAppLockManagerService().unlock(aInfo.packageName, pkg -> {
                     mContext.getMainExecutor().execute(() -> {
                         activityStarter.execute();
                     });
-                }, null /* cancelCallback */, mContext.getUserId());
+                }, null /* cancelCallback */, userId);
                 return ActivityManager.START_ABORTED;
             }
         }
@@ -1758,8 +1758,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
         }
         final String packageName = getTaskPackageName(task);
         if (packageName != null) {
-            final int userId = mContext.getUserId();
-            if (getAppLockManagerService().requireUnlock(packageName, userId)) {
+            if (getAppLockManagerService().requireUnlock(packageName, task.mUserId)) {
                 getAppLockManagerService().unlock(packageName,
                 pkg -> {
                     mContext.getMainExecutor().execute(() -> {
@@ -1770,7 +1769,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                 pkg -> {
                     // Send user to recents
                     getStatusBarManagerInternal().showRecentApps(false);
-                }, userId);
+                }, task.mUserId);
                 return ActivityManager.START_ABORTED;
             }
         }
@@ -3599,7 +3598,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
         }
         final String packageName = getTaskPackageName(task);
         if (packageName != null && getAppLockManagerService().requireUnlock(
-                packageName, mContext.getUserId())) {
+                packageName, task.mUserId)) {
             return null;
         }
         // Don't call this while holding the lock as this operation might hit the disk.
@@ -5285,14 +5284,14 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
             final ActivityInfo aInfo = resolveActivityInfoForIntent(intents[0], resolvedTypes[0], userId,
                 realCallingUid);
             if (aInfo != null) {
-                if (getAppLockManagerService().requireUnlock(aInfo.packageName, mContext.getUserId())) {
+                if (getAppLockManagerService().requireUnlock(aInfo.packageName, userId)) {
                     getAppLockManagerService().unlock(aInfo.packageName, pkg -> {
                         mContext.getMainExecutor().execute(() ->
                             getActivityStartController().startActivitiesInPackage(uid, realCallingPid,
                                 realCallingUid, callingPackage, callingFeatureId, intents, resolvedTypes,
                                 resultTo, options, userId, validateIncomingUser, originatingPendingIntent,
                                 allowBackgroundActivityStart));
-                    }, null /* cancelCallback */, mContext.getUserId());
+                    }, null /* cancelCallback */, userId);
                     return ActivityManager.START_ABORTED;
                 }
             }
@@ -5313,7 +5312,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
             final ActivityInfo aInfo = resolveActivityInfoForIntent(intent, resolvedType, userId,
                 realCallingUid);
             if (aInfo != null) {
-                if (getAppLockManagerService().requireUnlock(aInfo.packageName, mContext.getUserId())) {
+                if (getAppLockManagerService().requireUnlock(aInfo.packageName, userId)) {
                     getAppLockManagerService().unlock(aInfo.packageName, pkg -> {
                         mContext.getMainExecutor().execute(() ->
                             getActivityStartController().startActivityInPackage(uid, realCallingPid,
@@ -5321,7 +5320,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                                 resultTo, resultWho, requestCode, startFlags, options, userId, inTask,
                                 reason, validateIncomingUser, originatingPendingIntent,
                                 allowBackgroundActivityStart));
-                    }, null /* cancelCallback */, mContext.getUserId());
+                    }, null /* cancelCallback */, userId);
                     return ActivityManager.START_ABORTED;
                 }
             }

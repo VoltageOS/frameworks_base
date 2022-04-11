3afb901f159ca712e35c2d546371b434c9980fa9
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:35:11 -0700

    SystemUI: Add API for runtime taskbar config
    Allow Launcher3 to override the taskbar state at runtime based on user
    preference.
    
    Change-Id: Ic2616ff974e98e03edbcdbdf677fa40b40b37abb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6440,6 +6440,11 @@ public final class Settings {
         @Readable
         public static final String KEYGUARD_QUICK_TOGGLES = "keyguard_quick_toggles";
 
+	/**
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.

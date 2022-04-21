37d6a93cc4aa531a24894bf06693350f485d8f51
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-21 15:20:05 +0800

    SleepMode: Improve location mode logic
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/voltage/VoltageUtils.java
+++ core/java/com/android/internal/util/voltage/VoltageUtils.java
@@ -41,7 +41,6 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.provider.Settings;
 import android.hardware.SensorPrivacyManager;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.content.Intent;
@@ -522,7 +521,6 @@ public class VoltageUtils {
         private NotificationManager mNotificationManager;
         private WifiManager mWifiManager;
         private SensorPrivacyManager mSensorPrivacyManager;
-        private LocationManager mLocationManager;
         private BluetoothAdapter mBluetoothAdapter;
         private int mSubscriptionId;
         private Toast mToast;
@@ -530,9 +528,9 @@ public class VoltageUtils {
         private boolean mSleepModeEnabled;
 
         private static boolean mWifiState;
-        private static boolean mLocationState;
         private static boolean mCellularState;
         private static boolean mBluetoothState;
+        private static int mLocationState;
         private static int mRingerState;
         private static int mZenState;
 
@@ -546,7 +544,6 @@ public class VoltageUtils {
             mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
             mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
             mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
-            mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
             mSensorPrivacyManager = (SensorPrivacyManager) mContext.getSystemService(Context.SENSOR_PRIVACY_SERVICE);
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             mSubscriptionId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
@@ -601,25 +598,14 @@ public class VoltageUtils {
             }
         }
 
-        private boolean isLocationEnabled() {
-            if (mLocationManager == null) {
-                mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-            }
-            try {
-                return mLocationManager.isLocationEnabledForUser(UserHandle.of(ActivityManager.getCurrentUser()));
-            } catch (Exception e) {
-                return false;
-            }
+        private int getLocationMode() {
+            return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF, UserHandle.USER_CURRENT);
         }
 
-        private void setLocationEnabled(boolean enable) {
-            if (mLocationManager == null) {
-                mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-            }
-            try {
-                mLocationManager.setLocationEnabledForUser(enable, UserHandle.of(ActivityManager.getCurrentUser()));
-            } catch (Exception e) {
-            }
+        private void setLocationMode(int mode) {
+            Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.LOCATION_MODE, mode, UserHandle.USER_CURRENT);
         }
 
         private boolean isBluetoothEnabled() {
@@ -738,8 +724,8 @@ public class VoltageUtils {
             final boolean disableLocation = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_LOCATION_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
             if (disableLocation) {
-                mLocationState = isLocationEnabled();
-                setLocationEnabled(false);
+                mLocationState = getLocationMode();
+                setLocationMode(Settings.Secure.LOCATION_MODE_OFF);
             }
 
             // Disable Sensors
@@ -798,8 +784,8 @@ public class VoltageUtils {
             // Enable Location
             final boolean disableLocation = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_LOCATION_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
-            if (disableLocation && mLocationState != isLocationEnabled()) {
-                setLocationEnabled(mLocationState);
+            if (disableLocation && mLocationState != getLocationMode()) {
+                setLocationMode(mLocationState);
             }
 
             // Enable Sensors

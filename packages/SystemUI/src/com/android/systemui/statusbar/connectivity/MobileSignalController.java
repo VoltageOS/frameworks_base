68ab266b3f934eaced9f80361801ca0446e990f9
Dmitrii <bankersenator@gmail.com>
2022-04-24 13:20:34 +0000

    fix mobile icons toggles
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -128,8 +128,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private FeatureConnector<ImsManager> mFeatureConnector;
     private int mCallState = TelephonyManager.CALL_STATE_IDLE;
     private boolean mShowVolteIcon;
-    private boolean mIsVowifiAvailable;
     private boolean mDataDisabledIcon;
+    private boolean mIsVowifiAvailable;
     private boolean mRoamingIconAllowed;
 
     private final MobileStatusTracker.Callback mMobileCallback =
@@ -595,8 +595,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mSubscriptionInfo.getSubscriptionId(),
                 mCurrentState.roaming,
                 sbInfo.showTriangle,
-                volteId,
-                mCurrentState.isDefault);
+                mCurrentState.isDefault,
+                volteId);
         callback.setMobileDataIndicators(mobileDataIndicators);
     }
 
@@ -605,9 +605,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         IconState qsIcon = null;
         CharSequence qsDescription = null;
 
-        boolean pm = mProviderModelSetting || mProviderModelBehavior;
         if (mCurrentState.dataSim) {
-            // If using provider model behavior, only show QS icons if the state is also default
+            if (mProviderModelBehavior) {
+                return new QsInfo(qsTypeIcon, qsIcon, qsDescription);
+            }
+
             if (mCurrentState.showQuickSettingsRatIcon() || mConfig.alwaysShowDataRatIcon) {
                 qsTypeIcon = dataTypeIcon;
             }
@@ -631,7 +633,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
         if (mProviderModelBehavior) {
             boolean showDataIconStatusBar = (mCurrentState.dataConnected || dataDisabled)
-                    && mCurrentState.dataSim;
+                    && (mCurrentState.dataSim && mCurrentState.isDefault);
             typeIcon =
                     (showDataIconStatusBar || mConfig.alwaysShowDataRatIcon) ? dataTypeIcon : 0;
             showDataIconStatusBar |= mCurrentState.roaming;

fdfe498b2fff15cdb4701f49b8975d79dbdb6c50
jhenrique09 <jhsv09@gmail.com>
2022-05-14 22:51:51 +0000

    Call setBrightness when user stops swiping on statusbar
    * This way brightness gets saved and changes are shown on quick settings brightness bar
    
    Change-Id: I5daf99d7c2c1bebf430902bdbfad9971142be93b
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -605,6 +605,7 @@ public class StatusBar extends SystemUI implements
     private boolean mBrightnessControl;
     private boolean mBrightnessChanged;
     private boolean mJustPeeked;
+    private float mCurrentBrightness;
 
     // Flags for disabling the status bar
     // Two variables becaseu the first one evidently ran out of room for new flags.
@@ -2225,6 +2226,7 @@ public class StatusBar extends SystemUI implements
         final float val = convertGammaToLinearFloat(
                 Math.round(value * GAMMA_SPACE_MAX),
                 mMinimumBacklight, mMaximumBacklight);
+        mCurrentBrightness = val;
         mDisplayManager.setTemporaryBrightness(mDisplayId, val);
         AsyncTask.execute(new Runnable() {
             @Override
@@ -2333,6 +2335,7 @@ public class StatusBar extends SystemUI implements
             if (mJustPeeked && mExpandedVisible) {
                 mNotificationPanelViewController.fling(10, false);
             }
+            mDisplayManager.setBrightness(mDisplayId, mCurrentBrightness);
         }
     }
 

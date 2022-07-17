49d9d6886620c5dcb4c74d939b6583c28e5e4ee7
Pranav Vashi <neobuddy89@gmail.com>
2022-07-18 01:07:06 +0530

    Skip less boring notification check when reTicker mode is active
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
@@ -87,6 +87,7 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     @VisibleForTesting
     protected boolean mUseHeadsUp = false;
     private boolean mLessBoringHeadsUp = false;
+    private boolean mReTicker = false;
     private TelecomManager mTm;
     private Context mContext;
 
@@ -199,7 +200,7 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
             return false;
         }
 
-        if (shouldSkipHeadsUp(entry)) {
+        if (!mReTicker && shouldSkipHeadsUp(entry)) {
             mLogger.logNoHeadsUpShouldSkipPackage(entry);
             return false;
         }
@@ -602,6 +603,11 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     @Override
     public void setUseLessBoringHeadsUp(boolean lessBoring) {
         mLessBoringHeadsUp = lessBoring;
+    }
+
+    @Override
+    public void setUseReticker(boolean reTicker) {
+        mReTicker = reTicker;
     }
 
     public boolean shouldSkipHeadsUp(NotificationEntry entry) {

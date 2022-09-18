e819a3eeeaaaf2af1ed4f8b52366b46ada87bf56
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-18 17:20:32 +0530

    SystemUI: Improve statusbar mobile type icons padding
    The new "Silk design" mobile icons introduced in [1]
    have 2dp of spacing inside it on each sides. This is
    quite unnecessary and looks ugly as the mobile layout
    already provides 2.5dp of padding.
    
    For the sake of consistency, remove this spacing using
    insets and rely only on padding. Add space only for
    the second SIM's mobile data type icon.
    
    While we're at it, use 4G icon instead of LTE by default
    to reduce space usage.
    
    [1]: 084c13c8216f6a899cd3eda04fc1d7acff3d1248 ("Update RAT icons to match Silk design")
    
    Change-Id: I4d703e37983ee45da2219936156028d5d40f31a0
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -282,6 +282,9 @@ public class StatusBarSignalPolicy implements SignalCallback,
         state.roaming = indicators.roaming && !mHideRoaming;
         state.activityIn = indicators.activityIn && mActivityEnabled;
         state.activityOut = indicators.activityOut && mActivityEnabled;
+        state.typeSpacerVisible = mMobileStates.size() > 1
+               && mMobileStates.get(1).subId == state.subId
+               && state.typeId != 0;
 
         if (DEBUG) {
             Log.d(TAG, "MobileIconStates: "
@@ -581,6 +584,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
         public boolean roaming;
         public boolean needsLeadingPadding;
         public CharSequence typeContentDescription;
+        public boolean typeSpacerVisible;
 
         private MobileIconState(int subId) {
             super();
@@ -602,7 +606,8 @@ public class StatusBarSignalPolicy implements SignalCallback,
                     && showTriangle == that.showTriangle
                     && roaming == that.roaming
                     && needsLeadingPadding == that.needsLeadingPadding
-                    && Objects.equals(typeContentDescription, that.typeContentDescription);
+                    && Objects.equals(typeContentDescription, that.typeContentDescription)
+                    && typeSpacerVisible == that.typeSpacerVisible;
         }
 
         @Override
@@ -628,6 +633,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
             other.roaming = roaming;
             other.needsLeadingPadding = needsLeadingPadding;
             other.typeContentDescription = typeContentDescription;
+            other.typeSpacerVisible = typeSpacerVisible;
         }
 
         private static List<MobileIconState> copyStates(List<MobileIconState> inStates) {
@@ -644,7 +650,8 @@ public class StatusBarSignalPolicy implements SignalCallback,
         @Override public String toString() {
             return "MobileIconState(subId=" + subId + ", strengthId=" + strengthId
                     + ", showTriangle=" + showTriangle + ", roaming=" + roaming
-                    + ", typeId=" + typeId + ", visible=" + visible + ")";
+                    + ", typeId=" + typeId + ", typeSpacerVisible=" + typeSpacerVisible
+                    + ", visible=" + visible + ")";
         }
     }
 }

7706bf8d4295c6d38bfbcdb387306003031cfafa
DennySPb <dennyspb@gmail.com>
2022-09-29 10:23:12 +0300

    AdvancedGestures: Use Voltage utils for screenshot action
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1133,7 +1133,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                VoltageUtils.takeScreenshot(true);
                 break;
             case 8: // Notification panel
                 ActionUtils.toggleNotifications();

2f2fa62970894eab19d8f9d1cd209181af9ad6d1
Danny Lin <danny@kdrag0n.dev>
2022-10-01 21:19:24 +0530

    SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    The dedicated Wi-Fi/cellular data QS tiles are partially working now
    that we've exposed them in the list of tile options, but they're very
    buggy and somewhat broken when the provider model (unified internet) is
    enabled.
    
    Allow the tiles to co-exist with InternetTile and the provider model
    (including both settings_provider_model and combined signal icons) by
    always including QS icons in Wi-Fi/data indicator callbacks, and add
    a new flag to indicate whether they should be shown in the internet
    tile.
    
    (cdesai, mkbestas: Port to 12L)
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -556,6 +556,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mSubscriptionInfo.getSubscriptionId(),
                 mCurrentState.roaming,
                 sbInfo.showTriangle,
+		mCurrentState.isDefault,
                 0);
         callback.setMobileDataIndicators(mobileDataIndicators);
         });
@@ -568,7 +569,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
         if (mCurrentState.dataSim) {
             // If using provider model behavior, only show QS icons if the state is also default
-            if (!mCurrentState.isDefault) {
+            if (mProviderModelBehavior) {
                 return new QsInfo(qsTypeIcon, qsIcon, qsDescription);
             }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/SignalCallback.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/SignalCallback.kt
@@ -125,7 +125,8 @@ data class WifiIndicators(
     @JvmField val activityOut: Boolean,
     @JvmField val description: String?,
     @JvmField val isTransient: Boolean,
-    @JvmField val statusLabel: String?
+    @JvmField val statusLabel: String?,
+    @JvmField val isDefault: Boolean
 ) {
     override fun toString(): String {
         return StringBuilder("WifiIndicators[")
@@ -137,6 +138,7 @@ data class WifiIndicators(
                 .append(",qsDescription=").append(description)
                 .append(",isTransient=").append(isTransient)
                 .append(",statusLabel=").append(statusLabel)
+                .append(",isDefault=").append(isDefault)
                 .append(']').toString()
     }
 }
@@ -155,6 +157,7 @@ data class MobileDataIndicators(
     @JvmField val subId: Int,
     @JvmField val roaming: Boolean,
     @JvmField val showTriangle: Boolean,
+    @JvmField val isDefault: Boolean,
     @JvmField val volteId: Int
 ) {
     override fun toString(): String {
@@ -172,6 +175,7 @@ data class MobileDataIndicators(
                 .append(",roaming=").append(roaming)
                 .append(",showTriangle=").append(showTriangle)
                 .append(",volteId=").append(volteId)
+                .append(",isDefault=").append(isDefault)
                 .append(']').toString()
     }
 }

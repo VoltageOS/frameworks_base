781fd5f1fec6798425034d8afc4117d88952cf6f
Oliver Scott <olivercscott@gmail.com>
2022-11-26 00:10:31 +0100

    fixup! Restricted Networking Mode fixes
    The PACKAGE_ADDED broadcast which would add the profile owner app to the restricted networking mode allowlist is not triggered since the app's installation happens before the user is started.
    Add the app to the allowlist in the provided OEM profile provisioning callback
    
    Change-Id: Ie68a6921292fb2852743752ea298be7d61c2a016
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -17750,7 +17750,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
                 }
             }
 
-            onCreateAndProvisionManagedProfileCompleted(provisioningParams);
+            onCreateAndProvisionManagedProfileCompleted(userInfo.id, provisioningParams);
 
             sendProvisioningCompletedBroadcast(
                     userInfo.id,
@@ -17822,8 +17822,17 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
      *
      * <p>This method is meant to be overridden by OEMs.
      */
-    private void onCreateAndProvisionManagedProfileCompleted(
-            ManagedProfileProvisioningParams provisioningParams) {}
+    private void onCreateAndProvisionManagedProfileCompleted(int userId,
+            ManagedProfileProvisioningParams provisioningParams) {
+        try {
+            Set<Integer> uids = ConnectivitySettingsManager.getUidsAllowedOnRestrictedNetworks(
+                    mContext);
+            uids.add(mContext.getPackageManager().getPackageUidAsUser(
+                    provisioningParams.getOwnerName(), userId));
+            ConnectivitySettingsManager.setUidsAllowedOnRestrictedNetworks(mContext, uids);
+        } catch (NameNotFoundException ignored) {
+        }
+    }
 
     private void maybeInstallDevicePolicyManagementRoleHolderInUser(int targetUserId) {
         String devicePolicyManagerRoleHolderPackageName =

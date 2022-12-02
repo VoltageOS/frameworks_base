399c20e05cdf8ab62f50c58d2b7adc63db51c21c
Dmitrii <bankersenator@gmail.com>
2022-12-02 21:53:10 +0000

    Revert "BatteryStatsImpl: Stop resetting battery stats after reboot"
    This reverts commit 3f14ea992629cb8cf72f0336ccf73e9840fb5e31.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/os/BatteryStatsImpl.java
+++ core/java/com/android/internal/os/BatteryStatsImpl.java
@@ -15505,7 +15505,7 @@ public class BatteryStatsImpl extends BatteryStats {
             boolean reset = false;
             if (!mNoAutoReset && mSystemReady
                     && (oldStatus == BatteryManager.BATTERY_STATUS_FULL
-                    || level >= 99
+                    || level >= 90
                     || (mDischargeCurrentLevel < 20 && level >= 80)
                     || getHighDischargeAmountSinceCharge() >= 200)) {
                 Slog.i(TAG, "Resetting battery stats: level=" + level + " status=" + oldStatus

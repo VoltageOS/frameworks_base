979754dbb4b290d8b497199ed07c543d4bebb1e4
ShevT <evgeny.shishkov@gmail.com>
2022-12-19 16:24:06 +0300

    SystemUI: Add Screenshot tile
        OmniROM:
    
        From: maxwen <max.weninger@gmail.com>
        Date: Wed, 14 Oct 2020 17:17:31 +0200
        Subject: [PATCH] SystemUI: qs tiles revenge
    
        -screenshot
    
        [micky387]
         * edit to the new A12 Tiles API
         * Add secondaryLabel for the long press screenshot tile
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 14 Dec 2022 13:12:26 +0100
        Subject: [PATCH] SystemUI: Update ScreenshotTile after merge
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
@@ -30,6 +30,7 @@ import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.SleepModeTile
+import com.android.systemui.qs.tiles.ScreenshotTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VPNTetheringTile
@@ -87,6 +88,12 @@ interface VoltageModule {
     @IntoMap
     @StringKey(RefreshRateTile.TILE_SPEC)
     fun bindRefreshRateTile(refreshRateTile: RefreshRateTile): QSTileImpl<*>
+
+    /** Inject ScreenshotTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ScreenshotTile.TILE_SPEC)
+    fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds

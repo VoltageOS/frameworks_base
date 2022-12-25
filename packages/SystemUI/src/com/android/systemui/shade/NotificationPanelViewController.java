a8bfc1b153edbbb19a738c4db587ffc9cbb3ba5f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-26 03:22:20 +0530

    SystemUI: Combined QS header layout fixes
    Fixes:
    - Too low margin above QQS
    - Missing signal icons in QQS when >1 SIM
    - Privacy chip not clickable in QS
    - Too low padding between clock and date in QQS
    
    Change-Id: Ice4ddb8fab4542d386e65818c58172d1d30c03ef
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -621,6 +621,7 @@ public final class NotificationPanelViewController implements Dumpable {
     private boolean mQSAnimatingHiddenFromCollapsed;
     private boolean mUseLargeScreenShadeHeader;
     private boolean mEnableQsClipping;
+    private boolean mUseCombinedQSHeaders;
 
     private int mQsClipTop;
     private int mQsClipBottom;
@@ -1249,6 +1250,7 @@ public final class NotificationPanelViewController implements Dumpable {
 
         mUseLargeScreenShadeHeader =
                 LargeScreenUtils.shouldUseLargeScreenShadeHeader(mView.getResources());
+        mUseCombinedQSHeaders = mFeatureFlags.isEnabled(Flags.COMBINED_QS_HEADERS);
 
         mLargeScreenShadeHeaderHeight =
                 mResources.getDimensionPixelSize(R.dimen.large_screen_shade_header_height);
@@ -1262,7 +1264,9 @@ public final class NotificationPanelViewController implements Dumpable {
             mQuickQsHeaderHeight = SystemBarUtils.getQuickQsOffsetHeight(mView.getContext());
         }
         int topMargin = mUseLargeScreenShadeHeader ? mLargeScreenShadeHeaderHeight :
-                mResources.getDimensionPixelSize(R.dimen.notification_panel_margin_top);
+                mResources.getDimensionPixelSize(mUseCombinedQSHeaders
+                        ? R.dimen.notification_panel_margin_top_combined_headers
+                        : R.dimen.notification_panel_margin_top);
         mLargeScreenShadeHeaderController.setLargeScreenActive(mUseLargeScreenShadeHeader);
         mAmbientState.setStackTopMargin(topMargin);
         mNotificationsQSContainerController.updateResources();

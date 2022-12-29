58879144bbd37b0e242cabf9928c267fe2d3bcd0
Pranav Vashi <neobuddy89@gmail.com>
2022-12-29 19:40:30 +0530

    SystemUI: Clean up redundancy in BurnInProtection
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -48,13 +48,6 @@ public class PhoneStatusBarView extends FrameLayout {
     private static final String TAG = "PhoneStatusBarView";
     private final StatusBarContentInsetsProvider mContentInsetsProvider;
 
-    private int mBasePaddingBottom;
-    private int mBasePaddingLeft;
-    private int mBasePaddingRight;
-    private int mBasePaddingTop;
-
-    private ViewGroup mStatusBarContents;
-
     private DarkReceiver mBattery;
     private int mRotationOrientation = -1;
     @Nullable
@@ -82,14 +75,16 @@ public class PhoneStatusBarView extends FrameLayout {
     }
 
     public void shiftStatusBarItems(int horizontalShift, int verticalShift) {
-        if (mStatusBarContents == null) {
+        View sbContents = findViewById(R.id.status_bar_contents);
+
+        if (sbContents == null) {
             return;
         }
 
-        mStatusBarContents.setPaddingRelative(mBasePaddingLeft + horizontalShift,
-                mBasePaddingTop + verticalShift,
-                mBasePaddingRight + horizontalShift,
-                mBasePaddingBottom - verticalShift);
+        sbContents.setPaddingRelative(sbContents.getPaddingStart() + horizontalShift,
+                sbContents.getPaddingTop() + verticalShift,
+                sbContents.getPaddingEnd() + horizontalShift,
+                sbContents.getPaddingBottom() - verticalShift);
         invalidate();
     }
 
@@ -98,13 +93,6 @@ public class PhoneStatusBarView extends FrameLayout {
         super.onFinishInflate();
         mBattery = findViewById(R.id.battery);
         mCutoutSpace = findViewById(R.id.cutout_space_view);
-
-        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
-
-        mBasePaddingLeft = mStatusBarContents.getPaddingStart();
-        mBasePaddingTop = mStatusBarContents.getPaddingTop();
-        mBasePaddingRight = mStatusBarContents.getPaddingEnd();
-        mBasePaddingBottom = mStatusBarContents.getPaddingBottom();
 
         updateResources();
     }
@@ -129,13 +117,6 @@ public class PhoneStatusBarView extends FrameLayout {
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-
-        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
-
-        mBasePaddingLeft = mStatusBarContents.getPaddingStart();
-        mBasePaddingTop = mStatusBarContents.getPaddingTop();
-        mBasePaddingRight = mStatusBarContents.getPaddingEnd();
-        mBasePaddingBottom = mStatusBarContents.getPaddingBottom();
 
         updateResources();
 

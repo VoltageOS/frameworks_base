86cb9785db08675db94a7fa825ca536b8f009877
Tobias Merkel <stylogey@gmail.com>
2023-01-16 21:52:46 +0100

    SystemUI: fix duplicated creation of KeyguardStatusBarViewController
    This commit reverts:
     - 29060a14b9a1e81587db6c46410e8da7b0e59f80
     - cad8f587fb7525204dad248ace1b8b1d2ee6b8cc
    
    Reason: Not needed anymore as the duplicated init call registered
    the icon manager twice, therefore the crash.
    
    Change-Id: If33fc556275086aa65850f21b50e9f2720ab7415
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1390,8 +1390,8 @@ public final class NotificationPanelViewController implements Dumpable {
         mView.removeView(mKeyguardStatusBar);
         mKeyguardStatusBar = (KeyguardStatusBarView) mLayoutInflater.inflate(
                 R.layout.keyguard_status_bar, mView, false);
-        mView.addView(mKeyguardStatusBar, statusIndex);
         mKeyguardStatusBar.setVisibility(isOnKeyguard() ? View.VISIBLE : View.INVISIBLE);
+        mView.addView(mKeyguardStatusBar, statusIndex);
 
         // When it's reinflated, this is centered by default. If it shouldn't be, this will update
         // below when resources are updated.

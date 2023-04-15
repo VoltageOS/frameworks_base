AOSP-whatever/platform_frameworks_base

commit 28fbc82a275145c7b51641029c15d6d6d9491303

Lockscreen MediaArt: hide if media is not playing
it's annoying to get the artwork even if the notification gets
swiped out and media is paused, because the media session is still alive

Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -609,7 +609,9 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             mScrimController.setHasBackdrop(hasArtwork);
         }
 
-        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        // show artwork only if the media is playing
+        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
+                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING

VoltageOS/frameworks_base

commit 53dba055be209974fb32be6fc913c4042d806c78

fwb: Fix lockscreen wallpaper not being set if different from the home one
this got broken with:
https://github.com/ezio84/abc_frameworks_base/commit/28fbc82a275145c7b51641029c15d6d6d9491303

thanks to anayw2001 who digged into commits chain and found
the root of the issue

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -657,9 +657,7 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             mScrimController.setHasBackdrop(hasArtwork);
         }
 
-        // show artwork only if the media is playing
-        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
-                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING
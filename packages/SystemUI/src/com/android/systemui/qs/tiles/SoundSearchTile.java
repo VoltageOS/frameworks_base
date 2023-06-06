04e5c3116eb3de7b5b7419e0e8c04013511f098a
Gustavo Mendes <gusttavo.me@outlook.com>
2023-06-06 15:03:07 -0300

    SoundSearchTile: Do not use Shazam
    - Shazam has its own tile that works great, no reason to duplicate that.
    
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
@@ -76,14 +76,8 @@ public class SoundSearchTile extends QSTileImpl<BooleanState> {
     @Override
     protected void handleClick(@Nullable View view) {
         mHost.collapsePanels();
-        // Shazam
-        if (VoltageUtils.isPackageInstalled(mContext, "com.shazam.android") || VoltageUtils.isPackageInstalled(mContext, "com.shazam.encore.android")) {
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.setAction("com.shazam.android.intent.actions.START_TAGGING");
-            mContext.startActivity(intent);
         // Soundhound
-        } else if (VoltageUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium") || VoltageUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
+        if (VoltageUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium") || VoltageUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.soundhound.android.ID_NOW_EXTERNAL");

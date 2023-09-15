9f4e15c5b466b4c9dec6e7e3ea7314199528a93f
Dhina17 <dhinalogu@gmail.com>
2023-09-15 14:18:30 +0530

    GameManagerService: Set device_config property on behalf of GameSpace
    GTS don't allow any app to have the android.permission.WRITE_DEVICE_CONFIG permission
    i.e we can't modify device_config property from our game space.
    
    So write the property to our custom Settings.Secure.GAME_OVERLAY.
    Then observe the value changes and set the property to the
    device_config from here.
    
    Since GameManagerService is a part of system_server,
    GTS will be happy.
    
    Change-Id: I0ebbcd6188411a583fa53904e6153482f342a03e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12296,6 +12296,15 @@ public final class Settings {
          */
         public static final String PULSE_SMOOTHING_ENABLED = "pulse_smoothing_enabled";
 
+	/**
+         * Our GameSpace can't write to device_config directly [GTS]
+         * Use this as intermediate to pass device_config property
+         * from our GameSpace to com.android.server.app.GameManagerService
+         * so we can set the device_config property from there.
+         * @hide
+         */
+        public static final String GAME_OVERLAY = "game_overlay";
+
         /**
          * Enable udfps detection even when screen is off
          * Default value is 0

843af90cc9d97e684b70e0e2116a512d20dd8605
Dmitrii <bankersenator@gmail.com>
2023-11-07 20:40:34 +0000

    Revert "GlobalActions: Add isReboot as a param for ShutdownUi."
    This reverts commit 79f6f8645c6bcdebd306942a306535ef74ceb213.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/ShutdownUi.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/ShutdownUi.java
@@ -98,7 +98,7 @@ public class ShutdownUi {
         window.setBackgroundDrawable(background);
         window.setWindowAnimations(com.android.systemui.R.style.Animation_ShutdownUi);
 
-        d.setContentView(getShutdownDialogContent(isReboot));
+        d.setContentView(getShutdownDialogContent());
         d.setCancelable(false);
 
         int color;
@@ -129,12 +129,7 @@ public class ShutdownUi {
         d.show();
     }
 
-    /**
-     * Returns the layout resource to use for UI while shutting down.
-     * @param isReboot Whether this is a reboot or a shutdown.
-     * @return
-     */
-    public int getShutdownDialogContent(boolean isReboot) {
+    public int getShutdownDialogContent() {
         return R.layout.shutdown_dialog;
     }
 

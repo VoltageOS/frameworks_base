1bf570354f191fb79381afc0165b7d07f47ff44f
Pranav Vashi <neobuddy89@gmail.com>
2023-12-28 07:25:14 +0530

    Revert "VolumeDialog: Ensure proper resource release"
    This reverts commit b83e3314ab1a9cb0da5ed812f5abfc38dd7b5423.
    
    Log:
    
    FATAL EXCEPTION: main
    Process: com.android.systemui, PID: 990
    java.lang.NullPointerException: Attempt to invoke virtual method 'android.view.LayoutInflater android.app.Dialog.getLayoutInflater()' on a null object reference
     at com.android.systemui.volume.VolumeDialogImpl.initRow(VolumeDialogImpl.java:997)
     at com.android.systemui.volume.VolumeDialogImpl.addRow(VolumeDialogImpl.java:931)
     at com.android.systemui.volume.VolumeDialogImpl.onStateChangedH(VolumeDialogImpl.java:2311)
     at com.android.systemui.volume.VolumeDialogImpl$10.onStateChanged(VolumeDialogImpl.java:2810)
     at com.android.systemui.volume.VolumeDialogControllerImpl$C$3.run(VolumeDialogControllerImpl.java:965)
     at android.os.Handler.handleCallback(Handler.java:958)
     at android.os.Handler.dispatchMessage(Handler.java:99)
     at android.os.Looper.loopOnce(Looper.java:205)
     at android.os.Looper.loop(Looper.java:294)
     at android.app.ActivityThread.main(ActivityThread.java:8190)
     at java.lang.reflect.Method.invoke(Native Method)
     at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
     at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878)
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -445,10 +445,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     @Override
     public void onUiModeChanged() {
         mContext.getTheme().applyStyle(mContext.getThemeResId(), true);
-        if (mDialog != null) {
-            mDialog.dismiss();
-            mDialog = null;
-        }
     }
 
     public void init(int windowType, Callback callback) {
@@ -581,13 +577,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private void initDialog(int lockTaskModeState) {
         Log.d(TAG, "initDialog: called!");
-        if (mDialog != null) {
-            mDialog.dismiss();
-            mDialog = null;
-        }
-        if (mConfigurableTexts != null) {
-            mConfigurableTexts = null;
-        }
         mDialog = new CustomDialog(mContext);
         initDimens();
         adjustPositionOnScreen();
@@ -1758,7 +1747,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mHandler.removeMessages(H.DISMISS);
         rescheduleTimeoutH();
 
-        if (mDialog == null) {
+        if (mConfigChanged) {
             initDialog(lockTaskModeState); // resets mShowing to false
             mConfigurableTexts.update();
             mConfigChanged = false;
@@ -1874,7 +1863,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     if (mDialog != null) {
                         mDialog.dismiss();
                     }
-                    mDialog = null;
                     tryToRemoveCaptionsTooltip();
                     mExpanded = false;
                     if (mExpandRows != null) {
@@ -2787,11 +2775,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onConfigurationChanged() {
-            if (mDialog != null) {
-                mDialog.dismiss();
-                mDialog = null;
-                mConfigChanged = true;
-            }
+            mDialog.dismiss();
+            mConfigChanged = true;
         }
 
         @Override
